ca4a7223a30321d64010f3487fd7e321b2cb72ed
Iamsheye <seyepelumi@gmail.com>
2024-10-21 13:08:49 +0100

    chore/added color to input

--- src/components/tag-input.tsx
+++ src/components/tag-input.tsx
@@ -25,7 +25,7 @@ const TagInput = ({
           type="text"
           placeholder={placeholder}
           value={text}
-          className="h-[48px] w-full rounded-[24px] bg-form px-6 py-4 text-[0.875rem] font-semibold focus-visible:outline-form_text md:h-[88px] md:rounded-[40px] md:p-8 md:text-[1.25rem]"
+          className="h-[48px] w-full rounded-[24px] bg-form px-6 py-4 text-[0.875rem] font-semibold text-primary focus-visible:outline-form_text md:h-[88px] md:rounded-[40px] md:p-8 md:text-[1.25rem]"
           onChange={(e) => {
             setText(e.target.value);
           }}
