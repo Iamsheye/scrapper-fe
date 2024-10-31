5e508cd75b2b39e50102d66c1d3c321cc8b3a2ad
Iamsheye <seyepelumi@gmail.com>
2024-10-31 14:03:12 +0100

    feat: add verify email route and update error message in toast

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import toast from "react-hot-toast";
 
 export const toastError = (err: unknown) => {
   // @ts-ignore
-  const message = err.response?.data?.message || "Login Failed";
+  const message = err.response?.data?.message || "An error occured";
   toast.error(message, { duration: 7 * 1000 });
 };
 
