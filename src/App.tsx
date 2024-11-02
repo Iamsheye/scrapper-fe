ab31064f70a8af7411119f44978c28869bfb5334
Iamsheye <seyepelumi@gmail.com>
2024-11-02 09:08:28 +0100

    removed tanstack router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { lazy } from "react";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Toaster } from "react-hot-toast";
@@ -16,23 +15,10 @@ declare module "@tanstack/react-router" {
 
 const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-const TanStackRouterDevtools =
-  import.meta.env.NODE_ENV === "production"
-    ? () => null // Render nothing in production
-    : lazy(() =>
-        // Lazy load in development
-        import("@tanstack/router-devtools").then((res) => ({
-          default: res.TanStackRouterDevtools,
-          // For Embedded Mode
-          // default: res.TanStackRouterDevtoolsPanel
-        })),
-      );
-
 const App = () => {
   return (
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <RouterProvider router={router} />
-      <TanStackRouterDevtools initialIsOpen={false} router={router} />
       <Toaster />
     </GoogleOAuthProvider>
   );
